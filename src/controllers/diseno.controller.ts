7080699c46502c73ae8ba85df902c0acb45ec5c0
MiguelMiguez <miguelmiguezangel@gmail.com>
2026-08-12 14:25:08 -0300

    refactor: improve formatting of categoria assignment in getAllProducts function

--- src/controllers/diseno.controller.ts
+++ src/controllers/diseno.controller.ts
@@ -14,9 +14,8 @@ export const getAllProducts = async (
     };
 
     const zonaId = req.query.zonaId ? Number(req.query.zonaId) : undefined;
-    const categoria = typeof req.query.categoria === "string"
-      ? req.query.categoria
-      : undefined;
+    const categoria =
+      typeof req.query.categoria === "string" ? req.query.categoria : undefined;
 
     const result = await productService.getAll(pagination, zonaId, categoria);
     res.status(200).json(result);
